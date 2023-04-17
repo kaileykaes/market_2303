75b49af9bd7a740ff0e0890dec2f5ba78babb58a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-17 10:56:35 -0600

    Feat: #total_inventory

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,30 @@ class Market
       vendor.inventory.include?(item)
     end
   end
+
+  # def total_inventory
+  #   #vendors have inventories
+  #   big_daddy_inventory = Hash.new({:quantity => 0, :vendors => []})
+  #   @vendors.map do |vendor|
+  #     vendor.inventory.each do |item, amount|
+  #       big_daddy_inventory[item] unless big_daddy_inventory.has_key?(item)
+  #       big_daddy_inventory[item][:quantity] += amount unless big_daddy_inventory.has_key?(item)
+  #       big_daddy_inventory[item][:vendors] << vendor unless big_daddy_inventory[item][:vendors].include?(vendor) 
+  #     end
+  #   end
+  #   big_daddy_inventory
+  # end
+
+  def total_inventory  
+    errythang = {}
+    vendors.map do |vendor|
+      vendor.inventory.each do |item, _|
+        errythang[item] = {quantity: 0, vendors: []} unless errythang.has_key?(item)
+        errythang[item][:quantity] += vendor.inventory[item]
+        errythang[item][:vendors] << vendor unless errythang[item][:vendors].include?(vendor) 
+      end
+    end
+    errythang
+  end
+
 end
