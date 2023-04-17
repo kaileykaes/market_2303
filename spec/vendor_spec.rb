926ca5320755286f14f322272df31489b87eac69
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-17 09:11:20 -0600

    Test: #initialize

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+RSpec.describe Vendor do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  describe '#initialize' do 
+    it 'exists' do 
+      expect(@vendor).to be_a Vendor
+    end
+
+    it 'has attribues' do 
+      expect(@vendor.name).to eq('Rocky Mountain Fresh')
+      expect(@vendor.inventory).to eq({})
+    end
+  end
