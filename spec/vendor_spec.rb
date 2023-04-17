36d1a13985cc77c6c696fab56ebe2d3af6543899
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-17 09:14:48 -0600

    Test: #stock

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -22,5 +22,20 @@ RSpec.describe Vendor do
     it '#check_stock' do 
       expect(@vendor.check_stock(@item1)).to eq(0)
     end
+
+    it '#stock' do 
+      @vendor.stock(@item1, 30)
+      expect(@vendor.inventory).to eq({@item1 => 30})
+      expect(@vendor.check_stock(@item1)).to eq(30)
+      @vendor.stock(@item1, 25)
+      expect(@vendor.check_stock(@item1)).to eq(55)
+    end
+
+    it '#stock more than 1 item' do 
+      @vendor.stock(@item1, 30)
+      @vendor.stock(@item1, 25)
+      @vendor.stock(@item2, 12)
+      expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
+    end
   end
 end
